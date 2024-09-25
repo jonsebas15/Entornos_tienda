f3f26c8b6aa510a8f1abeaf2039e22d73b546edc
Johan Villamizar <johan_1115@hotmail.com>
2024-09-25 14:25:44 -0500

    pequeños cambios como cors

--- src/main/java/uis/edu/entorno/tienda/controlador/UsuarioController.java
+++ src/main/java/uis/edu/entorno/tienda/controlador/UsuarioController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import uis.edu.entorno.tienda.modelo.Usuario;
 import uis.edu.entorno.tienda.servicio.UsuarioService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 public class UsuarioController {
     @GetMapping("/")
     public String home() {
